9c2f74152f5d24e6fa70f64b6f911af3c464fdb8
Jessie Zhou <74047251+jessiezhoumd@users.noreply.github.com>
2020-12-21 17:04:04 +1100

    Update Plot6.R

--- Plot6.R
+++ Plot6.R
@@ -1,4 +1,4 @@
-## Q5. Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, California. Which city has seen greater changes over time in motor vehicle emissions?
+## Q6. Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, California. Which city has seen greater changes over time in motor vehicle emissions?
 ## A. Los Angeles County
 
 # Loading data
